e8d7af03852009987d982b4a872d6901b3dfdf41
Brett Bowley <58530824+Bearium-BB@users.noreply.github.com>
2026-09-04 00:35:54 -0500

    Add project files.

---
+++ Please_Choose_A_Password/Please_Choose_A_Password.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <conio.h> 
+
+std::string buildPassword()
+{
+    std::string password;
+	char ch;
+    while ((ch = _getch()) != '\r')
+    {
+        //This has to be inside of the while loop otherwise it will not work properly
+        //Something about the variable not being utilized
+        //ch = _getch();
+
+        if (ch == '\b') {
+            if (password.length() != 0) {
+                password.pop_back();
+                std::cout << "\b \b";
+            }
+        }
+        else
+        {
+            password += ch;
+            std::cout << '*';
+        }
+    }
+    std::cout << std::endl;
+    return password;
+}
+
+int main()
+{
+    std::string password;
+
+    bool isRunning = 1;
+
+
+    while (isRunning)
+    {
+        std::cout << "Enter password : ";
+
+        password = buildPassword();
+
+        if (!std::regex_search(password, std::regex("^.{8,}$")))
+        {
+            std::cout << "Password must be at least 8 characters long." << std::endl;
+        }
+        else if (!std::regex_search(password, std::regex("[a-z]")))
+        {
+            std::cout << "Password must contain at least one lowercase letter." << std::endl;
+        }
+        else if (!std::regex_search(password, std::regex("[A-Z]")))
+        {
+            std::cout << "Password must contain at least one uppercase letter." << std::endl;
+        }
+        else if (!std::regex_search(password, std::regex("[0-9]")))
+        {
+            std::cout << "Password must contain at least one number." << std::endl;
+        }
+        else
+        {
+            std::cout << "Password is valid. thank you!" << std::endl;
+            isRunning = 0;
+        }
+    }
+
+}
+
+//std::isupper(c)
+//std::islower(c)
+//std::isdigit(c)
+//password.length() 
